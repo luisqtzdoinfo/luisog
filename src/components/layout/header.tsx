ef82d39e98e5a300651a8e39ddd0419ee211dba6
Luis Gustavo <mariamarciasousa63@gmail.com>
2025-10-03 19:15:58 +0000

    luz mais forte  (_for element <LinkComponent>_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -37,7 +37,7 @@ export default function Header() {
           ))}
         </nav>
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <Button asChild className="shadow-lg shadow-primary/50 hover:shadow-primary/80 transition-shadow">
+          <Button asChild className="shadow-2xl shadow-primary/50 hover:shadow-primary/80 transition-shadow">
             <Link href="#pricing">Peça Agora</Link>
           </Button>
           <Sheet open={isSheetOpen} onOpenChange={setSheetOpen}>
